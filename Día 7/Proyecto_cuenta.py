d25c0f24ad6ac6b61ed396d5697f9dca8b532275
dPenedo <dpenedo@tutanota.com>
2022-05-11 12:43:50 -0300

    Día 7 termianadoOOo0o0o0o

--- Día 7/Proyecto_cuenta.py
+++ Día 7/Proyecto_cuenta.py
@@ -4,24 +4,49 @@ class Persona:
         self.apellido = apellido
 
 class Cliente(Persona):
-    def __init__(self, nombre, apellido, numero_cuenta, balance):
-        self.nombre = nombre
-        self.apellido = apellido    #  super().__init__(nombre, apellido)
+    def __init__(self, nombre, apellido, numero_cuenta, balance = 0):
+        super().__init__(nombre, apellido)
         self.numero_cuenta = numero_cuenta
         self.balance = balance
-    def imprimir_cliente(self):
-        print(f"El/la cliente {nombre} {apellido} de número de cuenta {numero_cuenta}, presenta el siguiente balance: \n {balance}")
+
+    def __str__(self):
+        return f"El/la cliente {self.nombre} {self.apellido} de número de cuenta {self.numero_cuenta}, presenta el siguiente balance: \n ${self.balance}"
     
-    def depositar(self, balance):
-        # dinero_a_ingresar = input("¿Cuánto dinero quieres ingresar? (ingresa la cifra y presiona Enter)")
-        # balance = 12
-        print(balance)
+    def depositar(self, monto_depositar):
+        self.balance += monto_depositar
+        print("Deposito aceptado")
         # return Cliente(balance) += dinero_a_ingresar
 
-cliente1 = Cliente('Daniel', 'Penedo', 21312, 999)
+    def retirar(self, monto_de_retiro):
+        if self.balance>= monto_de_retiro:
+            self.balance -= monto_de_retiro
+            print("Retiro realizado")
+        else:
+            print("Fondos insuficientes")
+
+def crear_cliente():
+    nombre_cl = input("Ingrese su nombre: ")
+    apellido_cl = input("Ingrese su Apellido: ")
+    numero_cuenta = input("Ingrese su numero de cuenta: ")
+    cliente = Cliente(nombre_cl, apellido_cl, numero_cuenta)
+    return cliente
+
+def comenzar():
+    mi_cliente = crear_cliente()
+    print(mi_cliente)
+    opcion = 0
+    while opcion != 'S':
+        print('Elije: Depositar (D), Retirar (R), o Salir (S)')
+        opcion = input ()
+        opcion= opcion.upper()
+        if opcion == 'D':
+            monto_dep = int(input('Monto a depositar: '))
+            mi_cliente.depositar(monto_dep)
+        elif opcion == 'R':
+            monto_ret = int(input('Monto a retirar: '))
+            mi_cliente.retirar(monto_ret)
+        print(mi_cliente)
+    print('Gracias por operar en aquesta mogudeta')
 
 
-
-
-
-# cliente1.depositar()
+comenzar()
