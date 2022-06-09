0459cda70ca10a57a944a0ff8e56b10f7f9b4d3d
dPenedo <dpenedo@tutanota.com>
2022-06-09 12:40:06 -0300

    Día 14 terminado

--- Día 14/asistencia.py
+++ Día 14/asistencia.py
@@ -1,7 +1,8 @@
 import cv2
 import face_recognition as fr
 import os
-
+import numpy
+from datetime import datetime
 # Crear base de datos
 
 ruta ='Empleados'
@@ -34,12 +35,29 @@ def codificar(imagenes):
     # Devolver lista codificada
     return lista_codificada
 
+# Registrar los ingresos
+
+def registrar_ingresos(persona):
+    f = open('registro.csv', 'r+')
+    lista_datos = f.readlines()
+    nombres_registro = []
+    for linea in lista_datos:
+       ingreso = linea.split(',')
+       nombres_registro.append(ingreso[0])
+
+    if persona not in nombres_registro:
+        ahora = datetime.now()
+        string_ahora = ahora.strftime('%H:%M:%S')
+        f.writelines(f'\n{persona}, {string_ahora}')
+
+
+
+
 lista_empleados_codificada = codificar(mis_imagenes)
 
 #Tomar una imagen de camara web
 
-captura = cv2.VideoCapture(2, cv2.CAP_ANY)
-
+captura = cv2.VideoCapture(0, cv2.CAP_ANY)
 # leer la imagen de la camara
 print(captura)
 exito, imagen = captura.read()
@@ -60,5 +78,29 @@ else:
         distancias = fr.face_distance(lista_empleados_codificada, caracodif)
 
         print(distancias)
+
+        indice_coincidencia = numpy.argmin(distancias)
+
+        # Mostrar coincidencias
+
+        if distancias[indice_coincidencia] > 0.6:
+            print('No coincide con ninguno de nuestros empleados')
+        else:
+            # Buscar nombre del empleado encontrado
+            nombre = nombres_empleados[indice_coincidencia]
+            y1, x2, y2, x1 = caraubic
+
+            cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(imagen, (x1, y2 -35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(imagen, nombre, (x1 +6, y2 -6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            
+            registrar_ingresos(nombre)
+
+             #Mostrar imagen obtenida
+            cv2.imshow('Imagen web', imagen)
+
+            # Mantener ventana abierta
+
+            cv2.waitKey(0)
 
 
